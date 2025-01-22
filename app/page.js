dac18305d6aaaff458f187b21747e522e7578da5
goldofman <goldofman@gmail.com>
2025-01-22 12:19:07 +0000

    update the hero section

--- app/page.js
+++ app/page.js
@@ -3,9 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Start now
-      </button>
+      <h1>Collect cstomers feedbacks to improve the product</h1>
+      <deiv>
+        Creat a feedback board in a minute to collect and prioritize the most
+        needed features for customers. They'll love it.
+      </deiv>
     </main>
   );
 }
