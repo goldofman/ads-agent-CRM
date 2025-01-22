3bebe060ce92cf77475929c15ea8088311b6c1c8
goldofman <goldofman@gmail.com>
2025-01-22 10:58:19 +0000

    change the button text

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Get started
+        Start now
       </button>
     </main>
   );
